9270c018830af6132b7ad0abc0e147c00701589a
manasa-s7 <17manasas@gmail.com>
2025-10-19 11:12:28 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from AWS CI/CD Pipeline!");
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
